d34097d9073fadcdbf275a514a0a14f02522c2fe
Shashank Khot <144458329+shshahank@users.noreply.github.com>
2025-01-09 23:40:36 +0530

    Add files via upload

---
+++ cppTemplate.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define vll vector <ll>
+#define vvi vector<vector<int>>
+#define vpll vector<pair<ll, ll>>
+#define pll pair<ll, ll>
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+    
+void solve() {
+    ll n; cin >> n;
+    vll arr(n);
+    for(ll i=0; i<n; i++) {cin >> arr[i];}
+    //sort(arr.begin(), arr.end());
+}
+    
+int main() {
+    ll t; cin >> t;
+    while(t--) {
+        solve();
+    }
+}
